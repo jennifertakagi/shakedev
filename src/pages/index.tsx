3927fd258330d10412d3851b60b6086619a5fc24
jennifertakagi <jennitakagi@gmail.com>
2021-02-23 22:16:35 -0300

    feat: mount document html and index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,28 @@
-import Head from 'next';
-import ExperienceBar from '../components/ExperienceBar';
+import Head from 'next/head';
+
+import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
+
+import styles from '../styles/pages/Home.module.css';
 
 function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Home | shake.dev</title>
+      </Head>
       <ExperienceBar />
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges/>
+          <Countdown />
+        </div>
+        <div>
+        </div>
+      </section>
     </div>
   );
 }
