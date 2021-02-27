1925f38426bb12c6fb35d8851b3f404c3d954f4b
jennifertakagi <jennitakagi@gmail.com>
2021-02-27 19:53:54 -0300

    feat: access cookies on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 
+import { ChallengesProvider } from '../contexts/ChallengeContext';
 import { CountdownProvider } from '../contexts/CountdownContext';
 
 import { ExperienceBar } from '../components/ExperienceBar';
@@ -10,29 +12,53 @@ import { ChallengeBox } from '../components/ChallengeBox';
 
 import styles from '../styles/pages/Home.module.css';
 
-function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Home | shake.dev</title>
-      </Head>
-      <ExperienceBar />
+interface HomeProps {
+  challengesCompleted: number;
+  currentExperience: number;
+  level: number;
+}
 
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges/>
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+function Home({ challengesCompleted, currentExperience, level }: HomeProps) {
+  return (
+    <ChallengesProvider
+      challengesCompleted={challengesCompleted}
+      currentExperience={currentExperience}
+      level={level}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Home | shake.dev</title>
+        </Head>
+        <ExperienceBar />
+
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges/>
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   );
 }
 
 export default Home;
 
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const userCookies = ctx.req.cookies['SHAKEDEV-userCookies'] || '{}';
+  const { challengesCompleted, currentExperience, level } = JSON.parse(userCookies);
+
+  return {
+    props: {
+      challengesCompleted: Number(challengesCompleted),
+      currentExperience: Number(currentExperience),
+      level: Number(level),
+    }
+  };
+}
